f34ecf4db19f7b4327dcbe280e769f27d1c4f5c7
MaiVincent <vincent.mai@umontreal.ca>
2021-08-30 18:18:19 -0400

    HouseCluster initialization

--- env/MA_DemandResponse.py
+++ env/MA_DemandResponse.py
@@ -1,5 +1,8 @@
 import gym
 import ray
+import pandas as pd
+from datetime import datetime, timedelta
+from datetime import time
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 from ray.rllib.utils.annotations import override, PublicAPI
 from ray.rllib.utils.typing import MultiAgentDict, AgentID
@@ -7,11 +10,17 @@ from typing import Tuple, Dict, List
 
 class MA_DemandResponseEnv(MultiAgentEnv):
 	"""Multi agent demand response environment"""
-	def __init__(self):
+	def __init__(self, env_properties):
 		super(MA_DemandResponseEnv, self).__init__()
-		self.agent_keys = ["house0", "house1","house2"]
 
-		
+		datetime_format = "%Y-%m-%d %H:%M:%S"
+		self.start_datetime = datetime.strptime(env_properties["start_datetime"], datetime_format) 	# Start date and time (Y,M,D, H, min, s)
+		self.datetime = self.start_datetime		# Start time in hour (24h format, decimal hours)
+		self.time_step = timedelta(seconds = env_properties["time_step"])
+		self.properties = env_properties
+		self.cluster = ClusterHouses(env_properties["cluster_properties"])
+		#self.power_grid = PowerGrid(power_grid_properties)
+
 	def reset(self):
 		obs_dict = {}
 		for agent in self.agent_keys:
@@ -20,47 +29,49 @@ class MA_DemandResponseEnv(MultiAgentEnv):
 
 
 	def step(self, action_dict):
-		obs_dict = {}
-		reward_dict = {}
-		dones_dict = {}
-		info_dict = {}
-		for agent in self.agent_keys:
-			if agent in action_dict.keys():
-				action = action_dict[agent]
-			else:
-				action = 0
-			obs_dict[agent] = [action] 		# Place-holder for agent observations
-			reward_dict[agent] = 1 			# Place-holder for agent reward
-			dones_dict[agent] = False 		# Place-holder for agent done
-			info_dict[agent] = []			# Place-holder for optional info value
+		pass
+		#obs_dict = {}
+		#reward_dict = {}
+		#dones_dict = {}
+		#info_dict = {}
+		#for agent in self.agent_keys:
+	#		if agent in action_dict.keys():
+	#			action = action_dict[agent]
+	#		else:
+	#			action = 0
+	#		obs_dict[agent] = [action] 		# Place-holder for agent observations
+	#		reward_dict[agent] = 1 			# Place-holder for agent reward
+	#		dones_dict[agent] = False 		# Place-holder for agent done
+	#		info_dict[agent] = []			# Place-holder for optional info value
 
 
-		return obs_dict, reward_dict, dones_dict, info_dict
+	#	return obs_dict, reward_dict, dones_dict, info_dict
 
-class HVAC(object)
+class HVAC(object):
 	""" HVAC simulator """
-	def __init__(self):
-		self.COP = 2.5 			# Coefficient of performance
-		self.cooling_cap = 1    # Cooling capacity
-		self.sensible_cooling_ratio = 0.75 # Ratio of sensible cooling/total cooling (vs latent cooling)
-		self.nominal_power = 10 # Power when on (W)
-		self.lockout_duration = 5 # Lockout duration (seconds)
-		self.turned_on = False  	# Status can be on or off
-		self.seconds_since_change = self.lockout_duration # Seconds since last turning off
+	def __init__(self, hvac_properties):
+		self.id = hvac_properties["id"]
+		self.COP = hvac_properties["COP"]										# Coefficient of performance (2.5)
+		self.cooling_capacity = hvac_properties["cooling_capacity"]    			# Cooling capacity (1)
+		self.sensible_cooling_ratio = hvac_properties["sensible_cooling_ratio"] # Ratio of sensible cooling/total cooling (vs latent cooling)
+		self.nominal_power = hvac_properties["nominal_power"] 					# Power when on (W) (10)
+		self.lockout_duration = hvac_properties["lockout_duration"] 			# Lockout duration (time steps) (5)
+		self.turned_on = False  												# HVAC can be on (True) or off (False)
+		self.steps_since_off = self.lockout_duration 							# Seconds since last turning off
 
 	def step(self, command):
-		if command = True:
-			if self.seconds_since_off >= self.lockout_duration: # Turn on if possible
+		if command == True:
+			if self.steps_since_off >= self.lockout_duration: # Turn on if possible
 				self.turned_on = True
-				self.seconds_since_off = 0
+				self.steps_since_off = 0
 			else:
 				self.turned_on = self.turned_on		# Otherwise, ignore command
 
 		else:
 			if self.turned_on:
-				self.seconds_since_off = 0  		# Start time counter
+				self.steps_since_off = 0  		# Start time counter
 			else:
-				self.seconds_since_off += 1			# Increment time counter
+				self.steps_since_off += 1			# Increment time counter
 			self.turned_on = False					# Turn off
 
 		if self.turned_on:
@@ -73,9 +84,58 @@ class HVAC(object)
 
 class SingleHouse(object):
 	""" Single house simulator """
-	def __init__(self):
-		self.Ua = 5  			#Place-holder for house conductance U_a ( )
-		self.Cm = 10000 		# Placeholder for house mass Cm (kg)
-		self.Ca = 20			# Placeholder for house air mass Ca (kg)
-		self.Hm = 1				# Placeholder for mass surface conductance Hm ( )
-		self.hvac = HVAC()
+	def __init__(self, house_properties):
+
+		"""
+		Initialize the house
+		"""
+		self.id = house_properties["id"] 	# Unique house ID
+		self.init_temp = house_properties["init_temp"]  # Initial temperature (Celsius degrees)
+		self.house_properties = house_properties 		# To keep in memory
+
+		# Thermal constraints
+		self.target_temp = house_properties["target_temp"] 	# Target temperature (Celsius degrees)
+		self.deadband = house_properties["deadband"]		# Deadband of tolerance around the target temperature (Celsius degrees)
+
+		# Thermodynamic properties
+		self.Ua = house_properties["Ua"]  			# House conductance U_a ( )
+		self.Cm = house_properties["Cm"] 			# House mass Cm (kg)
+		self.Ca = house_properties["Ca"]			# House air mass Ca (kg)
+		self.Hm = house_properties["Hm"]			# Mass surface conductance Hm ( )
+		
+		# HVACs
+		self.hvac_properties = house_properties["hvac_properties"]
+		self.hvacs = []
+		for hvac_prop in house_properties["hvac_properties"]:
+			hvac = HVAC(hvac_prop)
+			self.hvacs.append(hvac)
+
+	def step(self, action):
+		pass
+
+
+
+class ClusterHouses(object):
+	""" A cluster contains several houses, has the same outdoors temperature, and has one tracking signal """
+	def __init__(self, cluster_properties):
+		"""
+		Initialize the cluster of houses
+		"""
+		self.cluster_properties = cluster_properties
+
+		# Houses
+		houses = []
+		for house_properties in cluster_properties["houses_properties"]:
+			house = SingleHouse(house_properties)
+			houses.append(house)
+
+		# Outdoors temperature profile
+			## Currently modeled as noisy sinusoidal
+		self.day_temp = cluster_properties["day_temp"]
+		self.night_temp = cluster_properties["night_temp"]
+		self.temp_std = cluster_properties["temp_std"]  	# Std-dev of the white noise applied on outdoors temperature
+
+
+
+	def step(self, time, action):
+		pass
--- main.py
+++ main.py
@@ -3,14 +3,75 @@ from env import *
 
 
 
-env = MA_DemandResponseEnv()
+
+nb_houses = 10
+
+# Default properties:
+
+default_hvac_prop = {
+	"id": 1,
+	"COP": 2.5,									# Coefficient of performance (power spent vs heat displaced)
+	"cooling_capacity": 1,
+	"sensible_cooling_ratio": 0.75,				# Ratio of sensible cooling/total cooling (vs latent cooling)
+	"nominal_power": 10,						# In Watts
+	"lockout_duration": 1						# In number of steps (TODO: change to seconds)
+}
+
+default_house_prop = {
+	"id": 1,
+	"init_temp": 20,
+	"target_temp": 20,
+	"deadband": 2,
+	"Ua" : 1,									# House walls conductance (W/K)
+	"Cm" : 100000,								# House mass (kg)
+	"Ca" : 100,									# Air mass in the house (kg)
+	"Hm" : 2,									# House mass surface conductance (W/K)
+}
+
+# Creating houses
+houses_properties = []
+for i in range(nb_houses):
+	house_prop = default_house_prop
+	house_prop["id"] = str(i) 
+	hvac_prop = default_hvac_prop
+	hvac_prop["id"] = str(i) + "_1"
+	house_prop["hvac_properties"] = [hvac_prop]
+	houses_properties.append(house_prop)
+
+
+
+
+# Setting environment properties
+env_properties = {
+	"start_datetime": '2021-01-01 06:00:00',   	# Start date and time (Y-m-d H:M:S)
+	"time_step": 20,							# Time step in seconds
+	"cluster_properties": {
+		"day_temp": 30,							# Day temperature
+		"night_temp": 23,						# Night temperature
+		"temp_std": 0.5,						# Noise std dev on the temperature
+		"houses_properties": houses_properties
+	}
+
+}
+
+print(env_properties)
+
+print(env_properties["cluster_properties"]["houses_properties"][0]["hvac_properties"])
+
+
+env = MA_DemandResponseEnv(env_properties)
+
+
+
+
+
 
 # Testing environment
-action_dict = {
-	"house0": 0,
-	"house1": 1,
-	"house2": 2}
-obs = env.reset()
-print(obs)
-obs, rewards, dones, info  = env.step(action_dict)
-print([obs, rewards, dones, info])
+#action_dict = {
+#	"house0": 0,
+#	"house1": 1,
+#	"house2": 2}
+#obs = env.reset()
+#print(obs)
+#obs, rewards, dones, info  = env.step(action_dict)
+#print([obs, rewards, dones, info])
