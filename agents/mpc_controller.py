e2aa47e1e37481482b0b8b84b47c09db2b18749a
PhilippeM <philippe.maisonneuve@polymtl.ca>
2022-05-24 18:15:17 -0400

    read param from config dict

--- agents/mpc_controller.py
+++ agents/mpc_controller.py
@@ -1,11 +1,22 @@
+from distutils.log import error
+
+
 class MPCController(object):
     """Bang bang controller taking deadband into account: turns on when too hot, turns off when too cold, sticks to current state otherwise"""
 
     def __init__(self, agent_properties, config_dict):
         self.agent_properties = agent_properties
         self.id = agent_properties["id"]
-        print(config_dict)
-        raise ValueError('Error raised succeffully.')
+
+
+        self.mode = config_dict["default_env_prop"]["power_grid_prop"]["signal_mode"]
+
+        if self.mode == "sinusoidals":
+            self.signal_parameter = config_dict["default_env_prop"]["power_grid_prop"]["signal_parameters"]["sinusoidals"]
+
+        print(self.signal_parameter)
+        print(self.mode)
+        
         
 
     def agent_transition():
@@ -15,4 +26,8 @@ class MPCController(object):
         pass
 
     def act(self, obs):
+        print(obs)
+
+        raise error("Hey")
+        return 1
         pass
