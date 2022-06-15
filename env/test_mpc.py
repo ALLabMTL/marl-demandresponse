6e3fd1fec67d96dbf5b922f9ea9e6b0591bbf0c6
Philippe M <philippe.maisonneuve@polymtl.ca>
2022-06-15 17:05:00 -0400

    working MPC

--- env/test_mpc.py
+++ env/test_mpc.py
@@ -1,31 +1,32 @@
 from re import I
 from tabnanny import verbose
 import time
+
 import numpy as np
 import cvxpy as cp
 
 
-hvac_power = np.array([1] * 15)
-initial_air_temperature = np.array([24] * 15)
-initial_mass_temperature = np.array([24] * 15)
-target_temperature = np.array([20] * 15)
-remaining_lockout = [9, 9, 5, 5, 5, 5, 5, 5, 5, 5]
+hvac_power = np.array([10000] * 15)
+initial_air_temperature = np.array([10 + 273] * 15)
+initial_mass_temperature = np.array([10 + 273] * 15)
+target_temperature = np.array([20 + 273] * 15)
+remaining_lockout = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 temperature_extérieur = 20
 
 rolling_horizon = 15
 nb_agent = 10
 lockout_duration = 10
-signal = np.array([5] * rolling_horizon)
+signal = np.array([50000] * rolling_horizon)
 
 
 # ____________
 
 time_step_sec = 4
 Hm, Ca, Ua, Cm = (
-    np.array([1] * 15),
-    np.array([1] * 15),
-    np.array([1] * 15),
-    np.array([1] * 15),
+    np.array([2.84e03] * 15),
+    np.array([9.08e05] * 15),
+    np.array([2.18e02] * 15),
+    np.array([3.45e06] * 15),
 )
 
 # Convert Celsius temperatures in Kelvin
@@ -41,10 +42,11 @@ Qa = cp.Variable((rolling_horizon, nb_agent))
 # Variables and time constants
 a = [Cm[i] * Ca[i] / Hm[i] for i in range(15)]
 b = [Cm[i] * (Ua[i] + Hm[i]) / Hm[i] + Ca[i] for i in range(15)]
-
 c = [Ua[i] for i in range(15)]
-d = [Qa[i] + Ua[i] * od_temp_K for i in range(15)]
+d = cp.Variable((rolling_horizon, nb_agent))
 
+print(c)
+print(d)
 
 r1 = [(-b[i] + np.sqrt(b[i] ** 2 - 4 * a[i] * c[i])) / (2 * a[i]) for i in range(15)]
 r2 = [(-b[i] - np.sqrt(b[i] ** 2 - 4 * a[i] * c[i])) / (2 * a[i]) for i in range(15)]
@@ -73,35 +75,43 @@ temperature_difference = cp.Variable((rolling_horizon, nb_agent))
 
 
 constraints = [
-    consumption[i]
-    == cp.sum(cp.multiply(HVAC_state[i + lockout_duration], hvac_power[:nb_agent]))
-    for i in range(rolling_horizon)
+    consumption[t]
+    == cp.sum(cp.multiply(HVAC_state[t + lockout_duration], hvac_power[:nb_agent]))
+    for t in range(rolling_horizon)
 ]
+
+constraints += [
+    d[t][i] == Qa[t][i] + Ua[i] * od_temp_K
+    for t in range(rolling_horizon)
+    for i in range(nb_agent)
+]
+
 constraints += [
     Qa[t][i] == HVAC_state[t][i] * hvac_power[i] + other_Qa[t]
     for t in range(rolling_horizon)
     for i in range(nb_agent)
 ]
+
 constraints += [
     dTA0dt[t][i]
     == Hm[i] * mass_temperature[t][i] / Ca[i]
     - (Ua[i] + Hm[i]) * air_temperature[t][i] / Ca[i]
     + Ua[i] * od_temp_K / Ca[i]
-    + Qa[i] / Ca[i]
+    + Qa[t][i] / Ca[i]
     for t in range(rolling_horizon)
     for i in range(nb_agent)
 ]
 
 constraints += [
     A1[t][i]
-    == (r2[i] * air_temperature[t][i] - dTA0dt[t][i] - r2[i] * d[i] / c[i])
+    == (r2[i] * air_temperature[t][i] - dTA0dt[t][i] - r2[i] * d[t][i] / c[i])
     / (r2[i] - r1[i])
     for t in range(rolling_horizon)
     for i in range(nb_agent)
 ]
 
 constraints += [
-    A2[t][i] == air_temperature[t][i] - d[i] / c[i] - A1[i]
+    A2[t][i] == air_temperature[t][i] - d[t][i] / c[i] - A1[t][i]
     for t in range(rolling_horizon)
     for i in range(nb_agent)
 ]
@@ -110,24 +120,24 @@ constraints += [
     air_temperature[t + 1][i]
     == A1[t][i] * np.exp(r1[i] * time_step_sec)
     + A2[t][i] * np.exp(r2[i] * time_step_sec)
-    + d[i] / c[i]
-    for t in range(rolling_horizon - 1)
+    + d[t][i] / c[i]
+    for t in range(0, rolling_horizon - 1)
     for i in range(nb_agent)
 ]
 
 constraints += [
     mass_temperature[t + 1][i]
-    == A1[t][i] * A3[i] * cp.exp(r1[i] * time_step_sec)
-    + A2[t][i] * A4[i] * cp.exp(r2[i] * time_step_sec)
-    + d[i] / c[i]
-    for t in range(rolling_horizon - 1)
+    == A1[t][i] * A3[i] * np.exp(r1[i] * time_step_sec)
+    + A2[t][i] * A4[i] * np.exp(r2[i] * time_step_sec)
+    + d[t][i] / c[i]
+    for t in range(1, rolling_horizon - 1)
     for i in range(nb_agent)
 ]
 
 
 constraints += [HVAC_state[:lockout_duration] == past]
 constraints += [air_temperature[0] == initial_air_temperature[:nb_agent]]
-constraints += [air_temperature[0] == initial_mass_temperature[:nb_agent]]
+constraints += [mass_temperature[0] == initial_mass_temperature[:nb_agent]]
 constraints += [
     lockout_duration
     * (HVAC_state[i + lockout_duration, k] - HVAC_state[i + lockout_duration - 1, k])
@@ -144,8 +154,9 @@ constraints += [
     for k in range(nb_agent)
 ]
 constraints += [
-    temperature_difference[i] == (air_temperature[i] - target_temperature[:nb_agent])
-    for i in range(1, rolling_horizon)
+    temperature_difference[t][i] == (air_temperature[t][i] - target_temperature[i])
+    for t in range(i, rolling_horizon)
+    for i in range(nb_agent)
 ]
 
 problem = cp.Problem(
@@ -156,12 +167,14 @@ problem = cp.Problem(
     constraints,
 )
 
-print(cp.installed_solvers())
+
+# print(cp.installed_solvers())
 start = time.time()
 problem.solve(solver=cp.GUROBI, NodefileStart=0.5)
 
 end = time.time()
 print(problem.status)
+print(air_temperature.value - 273)
 print(HVAC_state.value)
 # print(temperature.value)
 
