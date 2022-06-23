605a88d7f31002bc758c10f2de79132f36eb952d
MaiVincent <vincent.mai@umontreal.ca>
2022-06-23 17:44:37 -0400

    Fix bug

--- agents/rl_controllers.py
+++ agents/rl_controllers.py
@@ -19,10 +19,7 @@ class PPOAgent():
 
         self.seed = agent_properties["net_seed"]
         torch.manual_seed(self.seed)
-        if True:
-            self.actor_net = OldActor(num_state=num_state, num_action=num_action)
-        else:
-            self.actor_net = Actor(num_state=num_state, num_action=num_action, layers = config_dict["nn_prop"]["actor_layers"])
+        self.actor_net = Actor(num_state=num_state, num_action=num_action, layers = config_dict["nn_prop"]["actor_layers"])
         self.actor_net.load_state_dict(torch.load(os.path.join(self.actor_path, 'actor.pth')))
         self.actor_net.eval()
 
