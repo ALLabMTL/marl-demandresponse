2313b11481c0a6b7d4a9c864ce9e6a44aa300b19
Mohamed Zakaria <mohamed-1.zakaria@polymtl.ca>
2023-01-28 14:59:00 -0500

    Finished general architecture of interface

---
+++ v1/client/src/app/components/grid-footer/grid-footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grid-footer',
+  templateUrl: './grid-footer.component.html',
+  styleUrls: ['./grid-footer.component.scss']
+})
+export class GridFooterComponent {
+
+}
