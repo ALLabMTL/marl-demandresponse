aaa4699a9b1d9688d1c658579ed7ec47c1886399
unknown <alex.hua@polymtl.ca>
2023-02-02 00:13:37 -0500

    first attempt at page change

--- v1/client/src/app/components/grid-footer/grid-footer.component.ts
+++ v1/client/src/app/components/grid-footer/grid-footer.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { GridComponent } from '../grid/grid.component';
 
 
 @Component({
@@ -8,22 +9,44 @@ import {MatIconModule} from '@angular/material/icon';
   styleUrls: ['./grid-footer.component.scss']
 })
 export class GridFooterComponent {
-
+  
+  @Output() pageChangeFromFooter:EventEmitter<any> = new EventEmitter;
+  
   currentPage: number = 1;
   maxPage: number = 35;
 
+  // constructor(public gridComponent:GridComponent){
+  //   super();
+  // }
+
   incrementPage(): void {
     if(this.currentPage < this.maxPage){
+      var page = (<HTMLInputElement>document.getElementById("myNumber")).value;
+      this.currentPage = parseInt(page);
       this.currentPage += 1;
+      console.log(this.currentPage);
     }
+    // let decrementValue = 1;
+    // this.pageChangeFromFooter.emit(decrementValue);
   }
 
-  decrementPage(): void{
+  decrementPage(): void {
     if(this.currentPage > 1) {
+      var page = (<HTMLInputElement>document.getElementById("myNumber")).value;
+      this.currentPage = parseInt(page);
       this.currentPage -= 1;
+      console.log(this.currentPage);
     }
+    // let decrementValue = -1;
+    // this.pageChangeFromFooter.emit(decrementValue);
+
     //(<HTMLInputElement>document.getElementById("myNumber")).value = "16";
   }
+  
 
 
 }
+function output() {
+  throw new Error('Function not implemented.');
+}
+
--- v1/client/src/app/components/grid/grid.component.ts
+++ v1/client/src/app/components/grid/grid.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Input, OnInit} from '@angular/core';
+
+interface PageData {
+  id: number;
+  content: string;
+}
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent {
+
+export class GridComponent implements OnInit {
+
+  @Input() pages: PageData[];
+  currentPage: number = 1;
+  maxPage: number = 35;
+
+  constructor(){
+    this.pages = [
+      { id: 1, content: "Page 1" },
+      { id: 2, content: "Page 2" },
+      { id: 3, content: "Page 3" },
+    ]
+  }
+
+  ngOnInit() {
+    this.currentPage = this.pages[0].id;
+  }
+
+  // pageChangeFromFooter(data:any){
+  //   console.log(data);
+  //   this.currentPage = this.currentPage - data;
+  // }
+
+  switchPage(id: number) {
+    this.currentPage = id;
+  }
 
 }
