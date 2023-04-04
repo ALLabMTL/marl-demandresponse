9415e2135b2d13d3d178299068126a51f88afff2
khouryjk <kh.jkhoury@gmail.com>
2023-04-04 15:12:41 -0400

    individual graphs for each house is done. Still need to fix graphs options for the zoom

--- client/src/app/components/dialog/dialog.component.ts
+++ client/src/app/components/dialog/dialog.component.ts
@@ -1,9 +1,13 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HouseData } from '@app/classes/sidenav-data';
 import { SharedService } from '@app/services/shared/shared.service';
 import { SimulationManagerService } from '@app/services/simulation-manager.service';
 import { GridComponent } from '../grid/grid.component';
+import { Chart, ChartConfiguration, ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import zoomPlugin from 'chartjs-plugin-zoom';
+Chart.register(zoomPlugin);
 
 
 @Component({
@@ -12,13 +16,98 @@ import { GridComponent } from '../grid/grid.component';
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent {
+  @ViewChild("a", { static: false }) // Works?
+  chartOne!: BaseChartDirective
+
   id: number;
   currentPage: number = 1;
-  
+
   // TODO: Do this in a prettier way
   constructor(@Inject(MAT_DIALOG_DATA) public data: number, public simulationManager: SimulationManagerService, public sharedService: SharedService) {
     this.id = data;
     this.sharedService.currentPageCount.subscribe(currentPage => this.currentPage = currentPage);
   }
+
+
+  ngAfterViewInit(): void {
+    // we HAVE to go though a subscribe because we need to call chart.update() to update the chart
+    this.simulationManager.houseDataObservable.subscribe((data) => {
+      {
+        //First graph
+        const categories = ['indoor_temp'];
+        let datasets = categories.map((category) => {
+          console.log(data)
+          console.log(this.id)
+          return {
+            data: data.map((e) => e.find((f) => f.id === this.id)?.indoorTemp).filter((val) => val !== undefined) as number[],
+            label: category,
+            fill: false,
+            tension: 0,
+            borderColor: ['blue'],
+            backgroundColor: ['blue'],
+            pointBackgroundColor: 'black',
+            pointRadius: 0,
+            pointHoverRadius: 15,
+          }
+        }
+        );
+        console.log("patate", datasets);
+        this.lineChartData.datasets = datasets;
+        //this.lineChartData.datasets = datasets;
+        this.lineChartData.labels = Array.from(Array(data.length).keys());
+      };
+      this.chartOne.chart?.update('none');
+    })
+
+  }
+
+  //First Graph
+  public lineChartData: ChartConfiguration<'line'>['data'] = {
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        label: 'Actual temperature',
+        fill: false,
+        tension: 0,
+        borderColor: 'black',
+        backgroundColor: 'rgba(255,0,0,0.3)'
+      }
+    ]
+  };
+
+
+  //Graphs options
+  public lineChartOptions: ChartOptions<'line'> = {
+    responsive: true,
+    display: true,
+    align: 'center',
+
+    plugins: {
+      zoom: {
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch: {
+            enabled: true,
+          },
+          mode: 'x',
+        },
+        pan: {
+          enabled: true,
+          mode: 'xy',
+        }
+      },
+      legend: {
+        display: true,
+        labels: {
+          color: 'black',
+          boxWidth: 5,
+          boxHeight: 5,
+        }
+      }
+    }
+  } as ChartOptions<'line'>;
 
 }
--- client/src/app/services/simulation-manager.service.ts
+++ client/src/app/services/simulation-manager.service.ts
@@ -14,7 +14,10 @@ interface PageData {
 export class SimulationManagerService {
 
   sidenavData: SidenavData[];
+  housesBufferData: HouseData[][] = [[]];
   sidenavObservable: Subject<SidenavData[]> = new Subject<SidenavData[]>();
+  houseDataObservable: Subject<HouseData[][]> = new Subject<HouseData[][]>();
+
 
   propertyNames: string[];
   propertyValues: string[];
@@ -59,43 +62,45 @@ export class SimulationManagerService {
     this.sharedService.currentPageCount.subscribe(currentPage => this.currentPage = currentPage);
 
 
-      this.sidenavData = [];
-      this.propertyNames = [];
-      this.propertyValues = [];
-      this.housesData = [];
+    this.sidenavData = [];
+    this.propertyNames = [];
+    this.propertyValues = [];
+    this.housesData = [];
 
-      this.started = true;
-      this.stopped = true;
-      this.pages = [];
-      this.houseDataFiltered = [];
-      this.originalHousesData = [];
-      this.maxPage = -1;
-      this.housesPerPage = 100;
+    this.started = true;
+    this.stopped = true;
+    this.pages = [];
+    this.houseDataFiltered = [];
+    this.originalHousesData = [];
+    this.maxPage = -1;
+    this.housesPerPage = 100;
 
-      this.isSortingSelected = false;
-      this.isHvacChecked = false;
-      this.isTempChecked = false;
-      this.isFilteredHvac = false;
-      this.isTempFiltered = false;
-      this.hvacStatus = " ";
-      this.minValueSliderInit= -1;
-      this.maxValueSliderInit= 1;
-      this.tempSelectRange = {min: this.minValueSliderInit, max: this.maxValueSliderInit }
-      this.tempSelectRangeInput = {min: this.minValueSliderInit, max: this.maxValueSliderInit }
+    this.isSortingSelected = false;
+    this.isHvacChecked = false;
+    this.isTempChecked = false;
+    this.isFilteredHvac = false;
+    this.isTempFiltered = false;
+    this.hvacStatus = " ";
+    this.minValueSliderInit = -1;
+    this.maxValueSliderInit = 1;
+    this.tempSelectRange = { min: this.minValueSliderInit, max: this.maxValueSliderInit }
+    this.tempSelectRangeInput = { min: this.minValueSliderInit, max: this.maxValueSliderInit }
 
-      this.sortingOptionSelected = " ";
-      this.hvacChosen = [];
-      this.tempDiffHousesData = [];
-      this.isHvacEnabled = false;
-      this.isOnChecked = false;
-      this.isOffChecked = false;
-      this.isLockoutChecked = false;
+    this.sortingOptionSelected = " ";
+    this.hvacChosen = [];
+    this.tempDiffHousesData = [];
+    this.isHvacEnabled = false;
+    this.isOnChecked = false;
+    this.isOffChecked = false;
+    this.isLockoutChecked = false;
   }
 
 
   addTimeStep(data: SidenavData): void {
     this.sidenavData.push(data);
     this.sidenavObservable.next(this.sidenavData);
+    this.housesBufferData.push(this.housesData);
+    this.houseDataObservable.next(this.housesBufferData);
     this.propertyNames = Object.getOwnPropertyNames(this.sidenavData[this.sidenavData.length - 1]);
     this.propertyValues = Object.values(this.sidenavData[this.sidenavData.length - 1]);
   }
@@ -108,24 +113,24 @@ export class SimulationManagerService {
 
     if (this.isSortingSelected) {
       this.sortByOptionSelected(this.sortingOptionSelected);
-    } 
-    if(this.isFilteredHvac) {
+    }
+    if (this.isFilteredHvac) {
       this.filterByHvacStatus(this.isHvacChecked, this.hvacStatus);
-    } 
+    }
 
     if (this.isTempFiltered) {
       this.filterByTempDiff();
-    } 
+    }
     this.updateFilteredHouses();
 
     this.tempSelectRange.min = this.originalHousesData.length > 0 ?
-    Math.min(...this.originalHousesData.map((data) => data.tempDifference)) :
-    0;
-    
+      Math.min(...this.originalHousesData.map((data) => data.tempDifference)) :
+      0;
+
     this.tempSelectRange.min = Number(this.tempSelectRange.min.toFixed(3));
 
     this.tempSelectRange.max = this.originalHousesData.length > 0 ?
-    Math.max(...this.originalHousesData.map((data) => data.tempDifference)) : 0;
+      Math.max(...this.originalHousesData.map((data) => data.tempDifference)) : 0;
 
     this.tempSelectRange.max = Number(this.tempSelectRange.max.toFixed(3));
 
@@ -161,24 +166,24 @@ export class SimulationManagerService {
       this.housesData = this.housesData.filter(x => {
         return this.houseDataFiltered.find(y => y.id === x.id) !== undefined;
       });
-    }  
+    }
 
-    if(this.isFilteredHvac) {
+    if (this.isFilteredHvac) {
       this.housesData = this.housesData.filter(x => {
         return this.hvacChosen.find(y => y.hvacStatus === x.hvacStatus) !== undefined;
-      });        
+      });
     }
 
     if (this.isTempFiltered) {
       this.housesData = this.housesData.filter(x => {
         return this.tempDiffHousesData.find(y => y.tempDifference === x.tempDifference) !== undefined;
-      }); 
+      });
     }
 
     // if(this.housesData.length === 0) {
     //   this.sharedService.changeCount(0);
     // }
-     if(this.currentPage > this.maxPage) {
+    if (this.currentPage > this.maxPage) {
       this.sharedService.changeCount(1);
     } else {
       this.sharedService.changeCount(this.currentPage);
@@ -248,17 +253,17 @@ export class SimulationManagerService {
     this.hvacStatus = hvac;
     this.isHvacChecked = checked;
 
-    if(this.isHvacChecked) {
+    if (this.isHvacChecked) {
       this.hvacChosen = [...this.hvacChosen, ...this.housesData.filter(status => status.hvacStatus == this.hvacStatus)];
     } else { // if un-select manually
       this.hvacChosen = [...this.hvacChosen.filter(x => x.hvacStatus !== this.hvacStatus)];
     }
 
-    this.isFilteredHvac = true;  
+    this.isFilteredHvac = true;
 
-    this.updateFilteredHouses(); 
-    
-    if(this.isOnChecked == false && this.isOffChecked == false && this.isLockoutChecked == false) {
+    this.updateFilteredHouses();
+
+    if (this.isOnChecked == false && this.isOffChecked == false && this.isLockoutChecked == false) {
       this.removeHvacFilter();
     }
   }
